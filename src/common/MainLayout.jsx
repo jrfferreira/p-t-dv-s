b533ded37fddd7297c1cb5d86edf99cad6da410a
joao.ferreira <joao.ferreira@corp.badoo.com>
2017-02-24 16:20:41 +0000

    Base style

--- src/common/MainLayout.jsx
+++ src/common/MainLayout.jsx
@@ -9,7 +9,7 @@ export default class MainLayout extends React.Component {
     return <div className="app-container">
               <header  className="app-container__header"><h1>p2p { this.props.params.hostId ? <span> session: <code>{this.props.params.hostId}</code></span> : null}</h1></header>
               <div className="app-container__body">{this.props.children}</div>
-              <footer className="app-container__footer">jrfferreira - 2017</footer>
+            <footer className="app-container__footer">jrfferreira - 2017</footer>
            </div>
   }
 }
--- src/session/Build.jsx
+++ src/session/Build.jsx
@@ -19,12 +19,19 @@ const defaultPeerState = {
   xt: 0
 }
 
+class Icon extends React.Component {
+  render(){
+    return <img src={`/bundle/img/${this.props.name}.png`} width={this.props.width||20} height={"auto"} className={`build__icon ${this.props.className}`}/>
+  }
+}
+
 class BooleanBuildIteraction extends React.Component {  
   
   props: {
     onUpdateBuild: PropTypes.Function,
     label: PropTypes.String,
     type: PropTypes.String,
+    readOnly: PropTypes.Boolean,
     value: PropTypes.Number,
     className: PropTypes.String,
   }
@@ -47,8 +54,9 @@ class BooleanBuildIteraction extends React.Component {
   }
 
   render() {
-    return <div className={this.getClasses()} onClick={this.props.onUpdateBuild ? this.onClick : null}>
-              {this.props.label}
+    return <div className={this.getClasses()} onClick={this.props.readOnly ? null : this.onClick}>
+              <Icon name={this.props.type}/>
+              {this.props.readOnly ? null : <div>{this.props.label}</div>}
             </div>
   }
 }
@@ -58,6 +66,7 @@ class CiclicBuildPiece extends React.Component {
   props: {
     onUpdateBuild: PropTypes.Function,
     type: PropTypes.String,
+    readOnly: PropTypes.Boolean,
     value: PropTypes.Number,
     className: PropTypes.String,
   }
@@ -84,8 +93,8 @@ class CiclicBuildPiece extends React.Component {
   }
 
   render() {
-    return <div className={this.getClasses()} onClick={this.props.onUpdateBuild ? this.onClick : null}>
-              {this.props.type}
+    return <div className={this.getClasses()} onClick={this.props.readOnly ? null : this.onClick}>
+              <Icon name={this.props.type}/>
             </div>
   }
 }
@@ -136,25 +145,25 @@ export class Build extends React.Component {
             </div>
             <div className="build__set">
               <div className="build__column">
-                <CiclicBuildPiece onUpdateBuild={this.onUpdateBuild} type="w1" value={this.state.build.w1}/>
-                <CiclicBuildPiece onUpdateBuild={this.onUpdateBuild} type="w2" value={this.state.build.w2}/>
-                <CiclicBuildPiece onUpdateBuild={this.onUpdateBuild} type="w3" value={this.state.build.w3}/>
+                <CiclicBuildPiece readOnly={this.props.readOnly} onUpdateBuild={this.onUpdateBuild} type="w1" value={this.state.build.w1}/>
+                <CiclicBuildPiece readOnly={this.props.readOnly} onUpdateBuild={this.onUpdateBuild} type="w2" value={this.state.build.w2}/>
+                <CiclicBuildPiece readOnly={this.props.readOnly} onUpdateBuild={this.onUpdateBuild} type="w3" value={this.state.build.w3}/>
               </div>
               <div className="build__column">
-                <CiclicBuildPiece onUpdateBuild={this.onUpdateBuild} type="c" value={this.state.build.c}/>
-                <CiclicBuildPiece onUpdateBuild={this.onUpdateBuild} type="m" value={this.state.build.m}/>
-                <CiclicBuildPiece onUpdateBuild={this.onUpdateBuild} type="k" value={this.state.build.k}/>
+                <CiclicBuildPiece readOnly={this.props.readOnly} onUpdateBuild={this.onUpdateBuild} type="c" value={this.state.build.c}/>
+                <CiclicBuildPiece readOnly={this.props.readOnly} onUpdateBuild={this.onUpdateBuild} type="m" value={this.state.build.m}/>
+                <CiclicBuildPiece readOnly={this.props.readOnly} onUpdateBuild={this.onUpdateBuild} type="k" value={this.state.build.k}/>
               </div>
               <div className="build__column">
-                <CiclicBuildPiece onUpdateBuild={this.onUpdateBuild} type="b" value={this.state.build.b}/>
-                <CiclicBuildPiece onUpdateBuild={this.onUpdateBuild} type="g" value={this.state.build.g}/>
-                <CiclicBuildPiece onUpdateBuild={this.onUpdateBuild} type="h" value={this.state.build.h}/>
+                <CiclicBuildPiece readOnly={this.props.readOnly} onUpdateBuild={this.onUpdateBuild} type="b" value={this.state.build.b}/>
+                <CiclicBuildPiece readOnly={this.props.readOnly} onUpdateBuild={this.onUpdateBuild} type="g" value={this.state.build.g}/>
+                <CiclicBuildPiece readOnly={this.props.readOnly} onUpdateBuild={this.onUpdateBuild} type="h" value={this.state.build.h}/>
               </div>
             </div>
             <div className="build__state">
-                <BooleanBuildIteraction onUpdateBuild={this.onUpdateBuild} label="need medicine" type="nm" value={this.state.build.nm}/>
-                <BooleanBuildIteraction onUpdateBuild={this.onUpdateBuild} label="ready to DZ" type="dz" value={this.state.build.dz}/>
-                <BooleanBuildIteraction onUpdateBuild={this.onUpdateBuild} label="ready to extract" type="xt" value={this.state.build.xt}/>
+                <BooleanBuildIteraction readOnly={this.props.readOnly} onUpdateBuild={this.onUpdateBuild} label="need medicine" type="nm" value={this.state.build.nm}/>
+                <BooleanBuildIteraction readOnly={this.props.readOnly} onUpdateBuild={this.onUpdateBuild} label="ready to DZ" type="dz" value={this.state.build.dz}/>
+                <BooleanBuildIteraction readOnly={this.props.readOnly} onUpdateBuild={this.onUpdateBuild} label="ready to extract" type="xt" value={this.state.build.xt}/>
             </div>
           </div>
   }
